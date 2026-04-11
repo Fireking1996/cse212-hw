b84b8173b27e4845ac7e6d8f085d73393e9d3a3b
Fireking1996 <firebringer1996@yahoo.com>
2026-04-11 11:21:17 -0500

    update

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,35 +11,57 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
-
         if (value < Data)
-        {
-            // Insert to the left
-            if (Left is null)
-                Left = new Node(value);
-            else
-                Left.Insert(value);
-        }
+    {
+        if (Left is null)
+            Left = new Node(value);
         else
-        {
-            // Insert to the right
-            if (Right is null)
-                Right = new Node(value);
-            else
-                Right.Insert(value);
-        }
+            Left.Insert(value);
+    }
+    else if (value > Data) // ✅ ONLY go right if greater
+    {
+        if (Right is null)
+            Right = new Node(value);
+        else
+            Right.Insert(value);
+    }
+    // ✅ if equal, do nothing (prevents duplicates)
     }
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
+        if (value == Data)
+    {
+        return true;
+    }
+    else if (value < Data)
+    {
+        if (Left is null)
+            return false;
+
+        return Left.Contains(value);
+    }
+    else
+    {
+        if (Right is null)
+            return false;
+
+        return Right.Contains(value);
+    }
         return false;
     }
 
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+       int leftHeight = 0;
+    int rightHeight = 0;
+
+    if (Left != null)
+        leftHeight = Left.GetHeight();
+
+    if (Right != null)
+        rightHeight = Right.GetHeight();
+
+    return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
